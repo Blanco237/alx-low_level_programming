c66b02d80b556e0acef5713ce781653f42c828ce
Blanco237 <asongrandy9@gmail.com>
2022-09-08 03:14:27 -0700

    Latest

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ *main - prints to stderr
+ *Return: 0 for success, 1 for failure
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
