08b372f4d66176694facb53b4f8b4239e1bf2e4d
Blanco237 <asongrandy9@gmail.com>
2022-09-09 12:59:41 -0700

    Latest

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - print alphabet in lower case
+ * Return: 0 for success, 1 for failure
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		putchar(i);
+	}
+	return (0);
+}
