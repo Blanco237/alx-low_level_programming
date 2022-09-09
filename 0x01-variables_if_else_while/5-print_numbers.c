d60e3dded7f6d24b52bd0a74983505a71bf338de
Blanco237 <asongrandy9@gmail.com>
2022-09-09 13:37:34 -0700

    Latest

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print numbers
+ * Return: 0 for success
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
