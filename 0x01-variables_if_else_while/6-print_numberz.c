b82f0b8173de4786cefaa854ee651d993c5ef539
Blanco237 <asongrandy9@gmail.com>
2022-09-10 03:01:30 -0700

    Latest

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print decimal numbers
+ * Return: 0 for success
+ */
+int main(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
