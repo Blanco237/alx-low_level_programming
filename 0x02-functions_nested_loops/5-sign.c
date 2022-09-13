596022c57732849059ef7f1b2c9eea9ed26a47a0
Blanco237 <asongrandy9@gmail.com>
2022-09-13 04:21:28 -0700

    Latest

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_sign - print the sign of a number
+ * @n: number to check
+ * Return: 1 for positive, 0 for zero, -1 for negative
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	_putchar('0');
+	return (0);
+}
