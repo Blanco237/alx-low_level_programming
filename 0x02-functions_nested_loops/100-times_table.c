6886c4ff1fd731dc5419e48f38528c7a023f3bab
Blanco237 <asongrandy9@gmail.com>
2022-09-13 05:16:47 -0700

    Latest

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_times_table - Entry point
+ * @n: input
+ * Return: Always 0 (Success)
+ */
+void print_times_table(int n)
+{
+	int i, j;
+	
+	if (n < 0 || n > 15)
+		return;
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			_putchar((i * j) + '0');
+			if (j == n)
+				continue;
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
